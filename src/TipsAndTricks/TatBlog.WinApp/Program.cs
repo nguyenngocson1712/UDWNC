edd327781038faa8075b4ca8d6dc4feb8106ef84
Nguyen Ngoc Son <2011432@dlu.edu.vn>
2023-03-02 17:28:56 +0700

    Update

--- src/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -74,7 +74,7 @@ IBlogRepository blogRepo = new BlogRepository(context);
 var pagingParams = new PagingParams
 {
     PageNumber = 1,
-    PageSize = 8,
+    PageSize = 5,
     SortColumn = "Name",
     SortOrder = "DESC"
 };
